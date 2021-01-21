44914225517364188f0d22d998fa11c24700698d
Nick <>
2021-01-21 12:47:05 +0300

    Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int initial_sum = 337;
-	int init_miles;
+	int InitialSum = 337;
+	int InitMiles;
 
-	init_miles=initial_sum/20;
-	System.out.println("Miles: "); System.out.println(init_miles);
+	InitMiles=InitialSum/20;
+	System.out.println("Miles: "); System.out.println(InitMiles);
     }
 }
