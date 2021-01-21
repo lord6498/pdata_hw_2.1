90e446a3a7fb8e88d17969888b74399dd252494b
Nick <>
2021-01-21 11:20:49 +0300

    Homework done

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int initial_sum = 337;
+	int init_miles;
+
+	init_miles=initial_sum/20;
+	System.out.println("Miles: "); System.out.println(init_miles);
+    }
+}
